8a04cbc8bc8699f6d9f07362b44f8122e9100392
VikashPalani <vikash1702palani@gmail.com>
2024-06-10 16:00:48 +0530

    till pattern 1

--- Patterns/patterns.cpp
+++ Patterns/patterns.cpp
@@ -1,7 +1,24 @@
-#include <iostream>
+#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
 
+/* Pattern 1
+****
+****
+****
+****
+*/
+
+void pattern1(){
+    for(int i=0;i<4;i++){
+        for(int j=0;j<4;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+
 int main(){
-    cout << "Hello World";
+    pattern1();
     return 0;
 }
